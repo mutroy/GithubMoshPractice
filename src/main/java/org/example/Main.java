78d410ed4b98db3d63f6a64c5a0cf727ce578ef1
MU <mericajam@gmail.com>
2023-06-07 14:35:29 -0400

    Update main for testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        for(int i=0;i<109;i++){
+            System.out.println("God is great");
+        }
     }
 }
